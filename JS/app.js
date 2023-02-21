53c77429318631ae57c69c37232c5747fd8b2691
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-21 22:54:12 +0330

    Feat: Toggling the mobile menu

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,11 @@
 const menuBtn = document.getElementById("menu-btn")
+const mobileMenu = document.getElementById("mobile-menu")
 
 const showMenu = () => {
     menuBtn.classList.toggle("open")
+    mobileMenu.classList.toggle("block")
+    mobileMenu.classList.toggle("hidden")
+
 }
 
 menuBtn.addEventListener("click" , showMenu)
